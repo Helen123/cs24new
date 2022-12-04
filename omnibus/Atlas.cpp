b5005a5e035915ff0d5b44017313740f50818945
吴奇 <wuqi@169-231-112-118.wireless.ucsb.edu>
2022-12-03 17:05:24 -0800

    update

--- omnibus/Atlas.cpp
+++ omnibus/Atlas.cpp
@@ -161,8 +161,8 @@ void Atlas::print(){
     }
   }
   }
-  delete e3;
-  e3=nullptr;
+  // delete e3;
+  // e3=nullptr;
   map<string,Edge*> nothing;
   //std::cout<<"have nothing"<<endl;
   return nothing;
@@ -176,6 +176,7 @@ string currentline;
 size_t size=stops.size();
 //std::cout<<stops["Meett"]->print()<<endl;
 map<string,Edge*> path=dijkstra(stops,size,src,dst);
+delete path[src];
 if(path.size()==0){
     throw std::runtime_error("No route.");
 }
